31cd262e4bbb4aeec40612592b8cce4325361785
M-U-U <faisaliqbalismail009@gmail.com>
2025-11-23 01:30:12 +0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Diperlukan untuk memproses JSX/React
 
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
+  base: process.env.VITE_BASH_PATH || "/frontend"
 });
