4442643b1bc1610c600365c9ca4766e67b92b065
M-U-U <faisaliqbalismail009@gmail.com>
2025-11-23 19:20:07 +0700

    Update test.jsx

--- src/test.jsx
+++ src/test.jsx
@@ -8,7 +8,7 @@ import TemperatureChart2 from './TemperatureChart2';
 import DataHistoris2 from './DataHistoris2';
 
 const Test = () => {
-    const SERVER_URL = 'http://bejer.tenzly.codes:14173/'; 
+    const SERVER_URL = 'http:localhost:5000'; 
 
     // State untuk menyimpan data
     // Menggunakan string '...' sebagai nilai awal yang lebih aman dan eksplisit.
